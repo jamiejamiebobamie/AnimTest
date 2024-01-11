8fd721e313aaf66c031b58c034758948103e117d
Jamie McCrory <jmccrory@vt.edu>
2024-01-11 17:23:10 -0500

    first commit

---
+++ Source/AnimTest/AnimTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AnimTest : ModuleRules
+{
+	public AnimTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
